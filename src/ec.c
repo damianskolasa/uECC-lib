a7a1ccacbe2d4374a42d64b252b455236146a2bf
fatfredyy <iop8@o2.pl>
2011-07-12 13:57:13 +0200

    Start changing

--- src/ec.c
+++ src/ec.c
@@ -5,3 +5,36 @@
  *      Author: damiank
  */
 
+#include "bn/bn.h"
+#include <stdio.h>
+
+
+int main(void){
+
+	BIGNUM *a = BN_new();
+	BIGNUM *b = BN_new();
+	BIGNUM *c = BN_new();
+	BN_init(a);
+	BN_init(b);
+	BN_init(c);
+
+
+
+	printf("%d\n", BN_dec2bn(&a, "350"));
+	printf("%s\n", BN_bn2dec(a));
+
+	printf("%d\n", BN_dec2bn(&b, "2"));
+	printf("%s\n", BN_bn2dec(b));
+
+	printf("%d\n", BN_mo(c, a, b, NULL));
+
+	printf("%s\n", BN_bn2dec(c));
+
+	BN_free(a);
+	BN_free(b);
+	BN_free(c);
+
+
+	return 0;
+
+}
